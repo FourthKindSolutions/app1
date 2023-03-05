e946bd2581969180dfabba982f8e9c5f78a3a1b2
4ks <107892265+4ks-devops@users.noreply.github.com>
2023-03-05 17:27:57 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['username'])) {
+    // Store the current page in a session variable
+    $_SESSION['redirect'] = $_SERVER['REQUEST_URI'];
+    header('Location: login.php');
+    exit;
+}
+
+// User is logged in, display the app content here
+$username = $_SESSION['username'];
+
+// Clear the redirect session variable if it exists
+if (isset($_SESSION['redirect'])) {
+    unset($_SESSION['redirect']);
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,7 +25,7 @@
 <body>
     <div class="container">
         <div class="embed-responsive embed-responsive-16by9">
-        <iframe width="100%" height="100%" class="embed-responsive-item" src="https://dashboard-csn.apps.oc.4ks.online" allowfullscreen></iframe>
+        <iframe width="100%" height="100%" class="embed-responsive-item" src="https://app1-csn.apps.oc.4ks.online" allowfullscreen></iframe>
         </div>
     </div>
 </body>
