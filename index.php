37b9d91dc92a7443261f5457bea02e6526971c94
4ks <107892265+4ks-devops@users.noreply.github.com>
2023-03-05 17:26:07 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
     <div class="container">
         <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item" src="https://dashboard-csn.apps.oc.4ks.online" allowfullscreen></iframe>
+        <iframe width="100%" height="100%" class="embed-responsive-item" src="https://dashboard-csn.apps.oc.4ks.online" allowfullscreen></iframe>
         </div>
     </div>
 </body>
