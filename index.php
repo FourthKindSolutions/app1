62a45841a9cdffb32ad40f3abbdf51553af79bfe
4ks <107892265+4ks-devops@users.noreply.github.com>
2023-03-05 17:22:21 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,13 @@
 <body>
     <h1>Welcome to App, <?php echo $username; ?>!</h1>
     <p>This is the content of App. You are logged in and authorized to access this page.</p>
-    
+    <!--INICIO del cont principal-->
+    <div class="container">
+        <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" src="https://dashboard-csn.apps.oc.4ks.online" allowfullscreen></iframe>
+        </div>
+    </div>
+       <!--FIN del cont principal-->
     <?php
     // Example link that redirects back to the previous page
     if (isset($_SESSION['redirect'])) {
