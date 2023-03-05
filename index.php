552acb7f063e2814b59fef92d4bea6d2e5b7174f
4ks <107892265+4ks-devops@users.noreply.github.com>
2023-03-05 16:46:33 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 
 // Check if the user is logged in
 if (!isset($_SESSION['username'])) {
-    header('Location: https://dashboard-csn.apps.oc.4ks.online/dashboard');
+    header('Location: /');
     exit;
 }
 
