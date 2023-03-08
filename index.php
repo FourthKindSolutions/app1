65c9e1cca227db83451b38588d5fe6bee70913ae
juangarcia4ks <107892265+juangarcia4ks@users.noreply.github.com>
2023-03-08 14:13:38 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
    <head>
       <script>
          window.onload = function() {
-            var iframe = document.getElementById("myiframe");
+            var iframe = document.getElementById("myiframee");
             iframe.width = iframe.contentWindow.document.body.scrollWidth;
             iframe.height = iframe.contentWindow.document.body.scrollHeight;
          }
       </script>
    </head>
    <body>
-      <iframe width="100%" height="100%" id="myiframe" src="https://4ks.mx"></iframe>
+      <iframe width="100%" height="100%" id="myiframee" src="https://4ks.mx"></iframe>
    </body>
 </html>
